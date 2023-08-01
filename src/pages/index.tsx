1ac37b7dc10fa0ec51f423882f2364b802a56d02
Ricardo Fonseca <ricardofsdomene@icloud.com>
2023-08-01 12:15:26 -0300

    2nd UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,6 @@ export default function Home() {
     return (
       <Flex p="20px" w="100%">
         <Flex
-          maxW="1000px"
           mx="auto"
           borderRadius={8}
           bg="#222"
@@ -67,99 +66,182 @@ export default function Home() {
 
   return (
     <>
-      <Flex flexDir="column" bg="#111" h="100vh" w="100%">
-        <Flex
-          pt="20px"
-          flexDir="column"
-          bg="#111"
-          h="100vh"
-          w="100%"
-          maxW="1000px"
-          mx="auto"
-          px={desktop ? 0 : "20px"}
-        >
-          <Image
-            bg="#000"
-            opacity={0.6}
-            borderTopRadius="20px"
-            h={mobile ? "100%" : "80vh"}
-            w="100%"
-            src="https://images.pexels.com/photos/1319458/pexels-photo-1319458.jpeg?cs=srgb&dl=pexels-nikolaos-dimou-1319458.jpg&fm=jpg"
-            objectFit="cover"
-          />
-          <Flex
-            p="20px"
-            zIndex={2}
-            flexDir="column"
-            justify="space-between"
-            mt="-20px"
-            mb="20px"
-            pt="20px"
-            bg="#FFF"
-            h="100%"
-            borderRadius="20"
-          >
-            <Flex flexDir="column">
-              <Text
-                style={{
-                  color: "#000",
-                  fontSize: mobile ? "2rem" : "2.6rem",
-                }}
-              >
-                Olá 👋
-              </Text>
-              <Text
-                style={{
-                  color: "#333",
-                  fontSize: mobile ? "1rem" : "1.3rem",
-                  fontFamily: "Poppins",
-                  fontWeight: 300,
-                }}
-              >
-                Pronto para agendar um horário?
-              </Text>
-            </Flex>
-            <Flex flexDir="column">
-              <Flex
-                bg="#333"
-                justify="center"
-                align="center"
-                borderRadius={8}
-                h="40px"
-                px="20px"
-              >
+      {mobile ? (
+        <Flex flexDir="column" bg="#111" h="100vh" w="100%">
+          <Flex flexDir="column" bg="#111" h="100vh" w="100%" mx="auto">
+            <Image
+              bg="#000"
+              opacity={0.6}
+              h={mobile ? "100%" : "80vh"}
+              w="100%"
+              src="https://images.pexels.com/photos/1319458/pexels-photo-1319458.jpeg?cs=srgb&dl=pexels-nikolaos-dimou-1319458.jpg&fm=jpg"
+              objectFit="cover"
+            />
+            <Flex
+              p="20px"
+              zIndex={2}
+              flexDir="column"
+              justify="space-between"
+              mt="-20px"
+              pt="20px"
+              bg="#FFF"
+              h="100%"
+              borderTopRadius="20"
+            >
+              <Flex flexDir="column">
                 <Text
-                  color="#FFF"
-                  fontWeight={200}
-                  fontSize="1rem"
-                  fontFamily="Poppins"
+                  style={{
+                    color: "#000",
+                    fontSize: mobile ? "2rem" : "2.6rem",
+                  }}
                 >
-                  Iniciar agendamento
+                  Olá 👋
+                </Text>
+                <Text
+                  style={{
+                    color: "#333",
+                    fontSize: mobile ? "1rem" : "1.3rem",
+                    fontFamily: "Poppins",
+                    fontWeight: 300,
+                  }}
+                >
+                  Pronto para agendar um horário?
                 </Text>
               </Flex>
-              <Flex
-                mt="10px"
-                bg="transparent"
-                justify="center"
-                align="center"
-                borderRadius={8}
-                h="40px"
-                px="20px"
-              >
-                <Text
-                textDecorationLine="underline"
-                  color="#333"
-                  fontWeight={200}
-                  fontSize="1rem"
-                  fontFamily="Poppins"
+              <Flex flexDir="column">
+                <Flex
+                  bg="#333"
+                  justify="center"
+                  align="center"
+                  borderRadius={8}
+                  h="40px"
+                  px="20px"
                 >
-                  Meus agendamentos
-                </Text>
+                  <Text
+                    color="#FFF"
+                    fontWeight={200}
+                    fontSize="1rem"
+                    fontFamily="Poppins"
+                  >
+                    Iniciar agendamento
+                  </Text>
+                </Flex>
+                <Flex
+                  mt="10px"
+                  bg="transparent"
+                  justify="center"
+                  align="center"
+                  borderRadius={8}
+                  h="40px"
+                  px="20px"
+                >
+                  <Text
+                    textDecorationLine="underline"
+                    color="#333"
+                    fontWeight={200}
+                    fontSize="1rem"
+                    fontFamily="Poppins"
+                  >
+                    Meus agendamentos
+                  </Text>
+                </Flex>
               </Flex>
             </Flex>
           </Flex>
         </Flex>
-      </Flex>
+      ) : (
+        <Flex flexDir="column" bg="#111" h="100vh" w="100%">
+          <Flex flexDir="column" bg="#111" h="100vh" w="100%" mx="auto">
+            <Image
+              bg="#000"
+              opacity={0.6}
+              h="100vh"
+              w="100%"
+              src="https://images.pexels.com/photos/1319458/pexels-photo-1319458.jpeg?cs=srgb&dl=pexels-nikolaos-dimou-1319458.jpg&fm=jpg"
+              objectFit="cover"
+            />
+            <Flex
+              h="100vh"
+              w="100%"
+              justify="center"
+              align="center"
+              position="absolute"
+            >
+              <Flex
+                p="20px"
+                zIndex={2}
+                flexDir="column"
+                justify="space-between"
+                mt="-20px"
+                pt="20px"
+                bg="#FFF"
+                minW={desktop ? "650px" :"500px"}
+                h="50%"
+                borderRadius="20"
+              >
+                <Flex flexDir="column">
+                  <Text
+                    style={{
+                      color: "#000",
+                      fontSize: mobile ? "2rem" : "2.6rem",
+                    }}
+                  >
+                    Olá 👋
+                  </Text>
+                  <Text
+                    style={{
+                      color: "#333",
+                      fontSize: mobile ? "1rem" : "1.3rem",
+                      fontFamily: "Poppins",
+                      fontWeight: 300,
+                    }}
+                  >
+                    Pronto para agendar um horário?
+                  </Text>
+                </Flex>
+                <Flex flexDir="column">
+                  <Flex
+                    bg="#333"
+                    justify="center"
+                    align="center"
+                    borderRadius={8}
+                    h="40px"
+                    px="20px"
+                  >
+                    <Text
+                      color="#FFF"
+                      fontWeight={200}
+                      fontSize="1rem"
+                      fontFamily="Poppins"
+                    >
+                      Iniciar agendamento
+                    </Text>
+                  </Flex>
+                  <Flex
+                    mt="10px"
+                    bg="transparent"
+                    justify="center"
+                    align="center"
+                    borderRadius={8}
+                    h="40px"
+                    px="20px"
+                  >
+                    <Text
+                      textDecorationLine="underline"
+                      color="#333"
+                      fontWeight={200}
+                      fontSize="1rem"
+                      fontFamily="Poppins"
+                    >
+                      Meus agendamentos
+                    </Text>
+                  </Flex>
+                </Flex>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Flex>
+      )}
     </>
   );
 }
